e6eb4aef4e5546e170adab342deb8a8b4aff67e5
imran-de <stimran4405@gmail.com>
2021-10-30 08:21:16 +0600

    banner-update

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -9,6 +9,9 @@ const Banner = () => {
                 <div className="container">
                     <div className="text-center">
                         <h1 className="display-1 text-light fw-bolder d-flex justify-content-center">JOIN THE FUN</h1>
+                        <p className="text-light text-center">
+                            It's never to early to start planning for the <br /> ween of a lifetime. keep scrolling to see what it's <br /> all about or browse our destination and dates <br /> to get booked now.
+                        </p>
                         <button onClick={() => history.push('allEvent')} className="btn btn-primary text-light fw-bold">Browse Now <i className="fas fa-anchor"></i></button>
                     </div>
                 </div>
