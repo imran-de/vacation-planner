4d976d4a7b1d9d7a0a017e83a477a55d00ac5977
imran-de <stimran4405@gmail.com>
2021-10-31 21:17:36 +0600

    modify

--- src/pages/Destination/Destination.js
+++ src/pages/Destination/Destination.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
 import { useParams } from 'react-router';
+import { HashLink } from 'react-router-hash-link';
 import useAuth from '../../hooks/useAuth';
 
 
@@ -62,24 +63,29 @@ const Destination = () => {
 
 
     return (
-        <div className="container mb-5">
-            <h2 className="fs-2 text-center">{destination?.eventName}</h2>
+        <div className="container-fluid mb-5">
 
-            <div className="my-5">
+            <div className="row">
+                <div className="col-12 text-end pb-2">
+                    {/* <HashLink to="#orderConfirm" className="btn btn-warning">Book Now</HashLink > */}
+                </div>
+                <div className="col-12 text-center mb-3 d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${destination?.imgURL})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '500px' }}>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <h2 className="fs-1 text-light fw-bolder">{destination?.eventName}</h2>
+                    </div>
+                    {/* <img className="img-fluid" src={destination?.imgURL} alt="" /> */}
+                </div>
+            </div>
+            <div className="container my-5">
                 <div className="row">
-                    <div className="col-12 text-end pb-2">
-                        <button className="btn btn-warning">Book Now</button>
-                    </div>
-                    <div className="col-12 text-center mb-3">
-                        <img className="img-fluid" src={destination?.imgURL} alt="" />
-                    </div>
+                    <h2 className="fs-3">{destination?.eventName}</h2>
                     <div className="col-md-6">
                         <h3><button className="btn btn-danger">{destination?.status} For Booking</button></h3>
                         <h5>Location: {destination?.location}</h5>
                         <h2>{destination?.eventName}</h2>
                         <p><b>Date: </b>{destination?.date}</p>
                         <p><b>Price: </b> From ${destination?.price} per person.</p>
-                        <button className="btn btn-warning">Confirmed Now</button>
+                        <HashLink to="#orderConfirm" className="btn btn-warning">Book Now</HashLink>
                     </div>
                     <div className="col-md-6">
                         <h4>Event Details: </h4>
@@ -88,46 +94,49 @@ const Destination = () => {
                 </div>
             </div>
             {/* order confirmed form */}
-            <div className="row justify-content-center my-5">
-                <h2 className="text-success text-center text-decoration-underline pb-4">For Confirm Booking Please Fill the Form</h2>
-                <div className="col-md-8">
-                    <form className="row g-3" onSubmit={handleSubmit(onSubmit)}>
-                        <div className=" col-md-3">
+            <div className="container">
+                <div id="orderConfirm" className="row justify-content-center my-5">
+                    <h2 className="text-success text-center text-decoration-underline pb-4">For Confirm Booking Please Fill the Form</h2>
+                    <div className="col-md-8">
 
-                            <input type="text" className="form-control" defaultValue={user?.displayName}  {...register("fullName")} placeholder="Full Name" required />
-                        </div>
-                        <div className=" col-md-3">
+                        <form className="row g-3" onSubmit={handleSubmit(onSubmit)}>
+                            <div className=" col-md-3">
 
-                            <input type="text" className="form-control"  {...register("contactNumber")} placeholder="contact no." required />
-                        </div>
-                        <div className=" col-md-4">
-                            <div className="input-group">
-                                <span className="input-group-text" id="inputGroupPrepend2">@</span>
-                                <input type="email" className="form-control" value={user?.email}  {...register("email")} placeholder="xyz@gmail.com" required readOnly />
+                                <input type="text" className="form-control" defaultValue={user?.displayName}  {...register("fullName")} placeholder="Full Name" required />
                             </div>
-                        </div>
-                        <div className="col-md-3">
-                            <input type="number" className="form-control"  {...register("person")} placeholder="Adult Person" required />
-                        </div>
-                        <div className="col-md-3">
-                            <input type="number" className="form-control"  {...register("chilled")} placeholder="chilled" required />
-                        </div>
-                        <div className="col-md-4">
-                            <input type="text" className="form-control" defaultValue={destination?._id}  {...register("confirmEventNo")} placeholder="Event" required readOnly />
-                        </div>
-                        <div className="col-10">
-                            <textarea type="text" className="form-control"  {...register("message")} placeholder="Message (optional)" />
-                        </div>
-                        <div className="col-12">
-                            <button className="btn btn-warning fw-bold" type="submit">Confirmed Now</button>
-                        </div>
-                    </form>
+                            <div className=" col-md-3">
+
+                                <input type="text" className="form-control"  {...register("contactNumber")} placeholder="contact no." required />
+                            </div>
+                            <div className=" col-md-4">
+                                <div className="input-group">
+                                    <span className="input-group-text" id="inputGroupPrepend2">@</span>
+                                    <input type="email" className="form-control" value={user?.email}  {...register("email")} placeholder="xyz@gmail.com" required />
+                                </div>
+                            </div>
+                            <div className="col-md-3">
+                                <input type="number" className="form-control"  {...register("person")} placeholder="Adult Person" required />
+                            </div>
+                            <div className="col-md-3">
+                                <input type="number" className="form-control"  {...register("chilled")} placeholder="chilled" required />
+                            </div>
+                            <div className="col-md-4">
+                                <input type="text" className="form-control" defaultValue={destination?._id}  {...register("confirmEventNo")} placeholder="Event" required readOnly />
+                            </div>
+                            <div className="col-10">
+                                <textarea type="text" className="form-control"  {...register("message")} placeholder="Message (optional)" />
+                            </div>
+                            <div className="col-12">
+                                <button className="btn btn-warning fw-bold" type="submit">Confirmed Now</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
+                {destination?.mapURL && <>
+                    <h3 className="text-center my-3 mb-5">Example itinerary for your week</h3>
+                    <iframe title={destination?._id} src={destination.mapURL} width="100%" height="450" style={{ border: "0" }} allowFullScreen="" loading="lazy"></iframe>
+                </>}
             </div>
-            {destination?.mapURL && <>
-                <h3 className="text-center my-3 mb-5">Example itinerary for your week</h3>
-                <iframe title={destination?._id} src={destination.mapURL} width="100%" height="450" style={{ border: "0" }} allowFullScreen="" loading="lazy"></iframe>
-            </>}
         </div>
     );
 };
