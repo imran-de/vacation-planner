050bd1aaee0fbd35e5628f1b121f2964baf1ce79
imran-de <stimran4405@gmail.com>
2021-10-31 21:34:51 +0600

    update

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import Subscribe from '../Home/Subscribe/Subscribe';
 
 const About = () => {
     return (
-        <div>
+        <div className="container-fluid">
             <div className='d-flex justify-content-center align-items-center' style={{ backgroundColor: "#F3F6F5", height: "300px" }}>
                 <h2 className="text-dark fw-bold">About Us</h2>
             </div>
-            <h2 className="text-center py-5">This is About page</h2>
+            <div className="container py-5">
+                <h2 className="text-info text-center">OUR COMPANY</h2>
+                <h1 className="text-center">Where we came from</h1>
+                <div className="row d-flex justify-content-center">
+                    <div className="col-md-6">
+                        <p className="text-center">
+                            Back in 1996 we were just a bunch of uni friends who wanted to go sailing and explore far and wide. Today, we’re still that same bunch of friends, only now there are 70,000 of us. And our loving family just keeps getting bigger. We have forged a community from adventurous spirits, vast open waters, and a host of musicians, collaborators, sailors and revellers.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            {/* add subscription component */}
+            <Subscribe />
         </div>
     );
 };
